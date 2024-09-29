b257169e4e570ba68eb40b7e663ae355821fed5c
Steve <falecomstevevaz@gmail.com>
2024-09-29 12:58:16 -0300

    Connecting to MongoDB with repository and service

--- src/main/java/com/steve/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/steve/workshopmongo/WorkshopmongoApplication.java
@@ -2,12 +2,12 @@ package com.steve.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.steve.workshopmongo.repository")
 public class WorkshopmongoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WorkshopmongoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopmongoApplication.class, args);
+    }
 }
